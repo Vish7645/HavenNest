df1fb883ca594f5c970cda6935f413c9fd1e8b34
Vishal Kumar <vk9319035@gmail.com>
2024-09-05 00:35:11 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 const express=require('express')
+const mongoose=require('mongoose')
+const dotenv = require("dotenv");
+
+dotenv.config({ path: '../.env' });
+
 const app=express()
 const PORT=3000
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Database Connected')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 
 
