15dad96751ce43d6f3c7f5715a448eb1d1a7ad73
Vishal Kumar <vk9319035@gmail.com>
2024-09-05 22:06:37 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,12 @@
 import User from './../models/user.model.js'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 import bcryptjs from 'bcryptjs'
+import dotenv from 'dotenv'
 
+
+dotenv.config({path:'./../../.env'})
 export const signup= async (req,res,next)=>{
-    // console.log(req.body)
     const {username,email,password}=req.body
     const hashedPassword=bcryptjs.hashSync(password,10)
     const newUser=new User({
@@ -15,6 +19,24 @@ export const signup= async (req,res,next)=>{
         res.status(201).json({massage:"User is successfully saved"})
     } catch (error) {
         // res.status(500).json(error.massage)
+        next(error)
+    }
+}
+
+export const signin=async (req,res,next)=>{
+    const {email,password} =req.body
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found'))
+        const validPassword=bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword) return next(errorHandler(401,'Wrong Credentials'))
+        const token=jwt.sign({_id: validUser._id}, process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token',token,{httpOnly : true})
+            .status(200)
+            .json(rest)
+    } catch (error) {
         next(error)
     }
 }
