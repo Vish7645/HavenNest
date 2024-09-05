427071a468ddb574a70112dfb20ae75574d138df
Vishal Kumar <vk9319035@gmail.com>
2024-09-05 22:20:22 +0530

    Adding signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,8 +30,12 @@ export const signin=async (req,res,next)=>{
         if(!validUser) return next(errorHandler(404,'User not found'))
         const validPassword=bcryptjs.compareSync(password,validUser.password)
         if(!validPassword) return next(errorHandler(401,'Wrong Credentials'))
+        
+        // We need JWT_SECRET as a secret agent to assign a stamp
         const token=jwt.sign({_id: validUser._id}, process.env.JWT_SECRET)
-        const { password: pass, ...rest } = validUser._doc;
+        
+        // To remove password showing at response
+        const { password: pass, ...rest } = validUser._doc; 
         res
             .cookie('access_token',token,{httpOnly : true})
             .status(200)
