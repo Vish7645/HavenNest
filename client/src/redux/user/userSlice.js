95133f8abc0048068506dc22f7a497f02ce83879
Vishal Kumar <vk9319035@gmail.com>
2024-09-07 01:26:45 +0530

    Complete upto update functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = {
   currentUser: null,
   error: null,
@@ -33,46 +34,16 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
-    deleteUserStart: (state) => {
-      state.loading = true;
-    },
-    deleteUserSuccess: (state) => {
-      state.currentUser = null;
-      state.loading = false;
-      state.error = null;
-    },
-    deleteUserFailure: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
-    signOutUserStart: (state) => {
-      state.loading = true;
-    },
-    signOutUserSuccess: (state) => {
-      state.currentUser = null;
-      state.loading = false;
-      state.error = null;
-    },
-    signOutUserFailure: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
   },
 });
 
 export const {
-    signInStart,
-    signInSuccess,
-    signInFailure,
-    updateUserFailure,
-    updateUserSuccess,
-    updateUserStart,
-    deleteUserFailure,
-    deleteUserSuccess,
-    deleteUserStart,
-    signOutUserFailure,
-    signOutUserSuccess,
-    signOutUserStart,
-  } = userSlice.actions;
-  
-  export default userSlice.reducer;
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+} = userSlice.actions;
+
+export default userSlice.reducer;
