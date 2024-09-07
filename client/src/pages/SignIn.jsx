a10a9bfc3e986effdcd6b3203d3d59e9b1390d39
Vishal Kumar <vk9319035@gmail.com>
2024-09-07 16:31:00 +0530

    SignOut functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -60,13 +60,13 @@ export default function Signin() {
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'  onChange={handleClick}/>
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'  onChange={handleClick}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading? 'Loading...':'Sign Up'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading? 'Loading...':'Sign in'}</button>
         <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
         <Link to={"/sign-up"}>
-          <span className='text-blue-700'>Sign in</span>
+          <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
   currentUser: null,
   error: null,
   loading: false,
 };
-
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -35,17 +33,29 @@ const userSlice = createSlice({
       state.loading = false;
     },
     deleteUserStart: (state) => {
-        state.loading = true;
-      },
-      deleteUserSuccess: (state) => {
-        state.currentUser = null;
-        state.loading = false;
-        state.error = null;
-      },
-      deleteUserFailure: (state, action) => {
-        state.error = action.payload;
-        state.loading = false;
-      },
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -59,6 +69,9 @@ export const {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;
