91a8cbd27ee5157d436fd030a2104815d96c38ba
Vishal Kumar <vk9319035@gmail.com>
2024-09-10 12:56:18 +0530

    Pre installation render

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config({ path: '../.env' });
 const app=express()
@@ -18,13 +19,23 @@ mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log('Database Connected')
 })
-
 .catch((err)=>{
     console.log(err)
 })
+
+const __dirname = path.resolve();
+
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500
     const message=err.message  || 'Internal Server Error'
